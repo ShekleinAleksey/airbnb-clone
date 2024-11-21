d78c83a0dcd3365170b92a791b945a5dbce412c3
ShekleinAl <sheklein.al@yandex.ru>
2024-11-21 22:15:54 +0300

    add handlers

--- handlers/advertisement.go
+++ handlers/advertisement.go
@@ -2,6 +2,26 @@ package handlers
 
 import "github.com/gin-gonic/gin"
 
-func advertisementHandler(c *gin.Context) {
+type Listing struct {
+	ID          int     `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Location    string  `json:"location"`
+	Price       float64 `json:"price"`
+}
+
+func GetListing(c *gin.Context) {
+
+}
+
+func CreateListing(c *gin.Context) {
+
+}
+
+func UpdateListing(c *gin.Context) {
+
+}
+
+func DeleteListing(c *gin.Context) {
 
 }
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"airbnb-clone/handlers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.GET("/listings", handlers.GetListing)
+	r.POST("/listings", handlers.CreateListing)
+	r.PUT("/listings/:id", handlers.UpdateListing)
+	r.DELETE("listings/:id", handlers.DeleteListing)
 
 	r.Run(":8080")
 }
